99e60e952c94481159885776b36b6e4244eba9f9
Atul Singh <atulsingh179@gmail.com>
2023-08-15 15:18:02 +0400

    Add files via upload
    This is the code of zero crossing detection

---
+++ zcd.m
@@ -0,0 +1,15 @@
+function[z] = zcd(y,a,l)
+% a is the starting index wherefrom zero crossigns detection will start
+% l is the end index up to which zero crossing detection will be performed
+z = [0];
+len = length(y);
+x = y(a:l);
+j = 1
+for i = 1:len(x)-1
+    a = x(i)*x(i+1);
+    if a<0
+        z(j) = i;
+        j = j+1;
+    end
+end
+% z is the instants of zero crossing
