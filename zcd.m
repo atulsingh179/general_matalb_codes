cf50b2ca3c4aae9a9d133aa4aad44ed864d82af1
Atul Singh <atulsingh179@gmail.com>
2023-08-16 17:31:06 +0400

    Update zcd.m

--- zcd.m
+++ zcd.m
@@ -1,15 +1,24 @@
-function[z] = zcd(y,a,l)
+function[z] = zcd(x,a,l) % x is the input signal
 % a is the starting index wherefrom zero crossigns detection will start
 % l is the end index up to which zero crossing detection will be performed
 z = [0];
-len = length(y);
 x = y(a:l);
-j = 1
-for i = 1:length(x)-1
+j = 1; i =1;
+x_len = length(x);
+while i<x_len
+    if abs(x(i))<1e-10 %removes the instances when a small values of same sign as previous value fall near to zero
+        x(i) = 0;
+    end
+    if abs(x(i+1))<1e-10
+        x(i+1) =0;
+    end
     a = x(i)*x(i+1);
-    if a<0
+    if a<0 || a==0
         z(j) = i;
         j = j+1;
+        i = i+1; % removes the instances where above condition satisfies for two consecutive values
     end
+    i=i+1;
 end
-% z is the instants of zero crossing
+end
+% z is the ZCD instants
